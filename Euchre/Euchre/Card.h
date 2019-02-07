60cdc384ab80f05f16408a6db4d95bab403e0f8a
haley.phillips <haleyphillips928@gmail.com>
2019-02-06 21:10:47 -0600

    classes added

---
+++ Euchre/Euchre/Card.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+
+class Card
+{
+private:
+	int m_value;
+	std::string m_name;
+public:
+	Card();
+	~Card();
+	Card(std::string name, int value);
+
+	int getValue();
+	void setValue(int value);
+	std::string getName();
+};
+
