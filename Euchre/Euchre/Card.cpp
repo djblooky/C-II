810b22e0ab5dd855b037dafd2f214770a406bb0d
Haley Phillips <haleyphillips928@gmail.com>
2019-02-11 11:39:36 -0600

    added card methods

--- Euchre/Euchre/Card.cpp
+++ Euchre/Euchre/Card.cpp
@@ -1,12 +1,23 @@
-#include "stdafx.h"
+#include "pch.h"
 #include "Card.h"
 
-
-Card::Card()
+Card::Card(std::string name, int value)
 {
+	m_name = name;
+	m_value = value;
 }
 
-
-Card::~Card()
+int Card::getValue()
 {
+	return m_value;
+}
+
+void Card::setValue(int value)
+{
+	m_value = value;
+}
+
+std::string Card::getName()
+{
+	return m_name;
 }
