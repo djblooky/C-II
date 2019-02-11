07f7f9f7a2f79ec881bac7f3e84efd8a855e68e9
Haley Phillips <haleyphillips928@gmail.com>
2019-02-11 11:50:23 -0600

    added deck vector and basic methods

--- Euchre/Euchre/Deck.h
+++ Euchre/Euchre/Deck.h
@@ -1,12 +1,19 @@
 #pragma once
+#include <vector>
 #include "Card.h"
+#include "Player.h"
 
 class Deck
 {
 private:
+	std::vector<Card> m_deck;
 	Card *nine, *ten, *jack, *queen, *king, *joker;
 public:
 	Deck();
 	~Deck();
+	void createDeck(); //makes four of each card type for 24 card deck and adds each one to m_deck vector
+	void deal(std::vector<Player> &players); //deals to each player
+	void shuffle();
+	
 };
 
